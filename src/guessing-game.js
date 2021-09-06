a956e13f36527af95c25f3e5c65214ecd9c734e0
Leytek <leytek@yandex.ru>
2021-09-06 18:33:56 +0300

    feat: implement guessing game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return this.g = Math.ceil((this.max + this.min) / 2);
     }
 
     lower() {
-
+        this.max = this.g;
     }
 
     greater() {
-
+        this.min = this.g;
     }
 }
 
